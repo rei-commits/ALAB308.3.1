fbb5bd6e7b6bf2b6f3d2f98f3bc127c65fd36bf9
ReiRei <125166401+rei-commits@users.noreply.github.com>
2024-09-20 16:12:48 -0400

    added comments to my js

--- part1.js
+++ part1.js
@@ -1,3 +1,7 @@
+// Loop through all numbers from 1 to 100 If a number is divisible by 3, log “Fizz.”
+// If a number is divisible by 5, log “Buzz.”
+// If a number is divisible by both 3 and 5, log “Fizz Buzz.”
+// If a number is not divisible by either 3 or 5, log the number.
 for(i = 1; i<=100; i++){
     if(i % 3 ===0 && i % 5 ===0){
         console.log("FizzBuzz");
